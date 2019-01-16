ab5c19e31a3fe22593b5f7cd63b74c540a49bdef
Reuben Cleetus <recleetu@microsoft.com>
2019-01-16 17:27:53 -0500

    implemented Read and Write to Key Vault

---
+++ Learn-KeyVault-Console/Helpers/KeyVaultHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn_KeyVault_Console.Helpers {
+    class KeyVaultHelper {
+        public KeyVaultHelper() { }
+
+        public async Task<string> GetSecret(string secretName) {
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            var secret = keyVaultClient.GetSecretAsync("https://reubenkv.vault.azure.net/secrets/" + secretName).GetAwaiter().GetResult();
+
+            return secret.Value;
+        }
+
+        public async void WriteSecret(string secretName, string secretValue) {
+            AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+            KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            await keyVaultClient.SetSecretAsync("https://reubenkv.vault.azure.net/", secretName, secretValue,null,"text");
+
+        }
+    }
+}
---
+++ Learn-KeyVault-Console/Program.cs
@@ -0,0 +1,58 @@
+﻿using Learn_KeyVault_Console.Helpers;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using System;
+
+namespace Learn_KeyVault_Console {
+    class Program {
+
+        static void Main(string[] args) {
+            string vaultName, secretName, secretValue = "";
+            KeyVaultHelper kvHelper = new KeyVaultHelper();
+            Program.printDefaultInstructions();
+            string input = "";
+            Program p = new Program();
+            input = Console.ReadLine().ToUpper();
+            while ((input != " ")) {
+                switch (input) {
+                    case "X":
+                        
+                        Environment.Exit(0);
+                        break;
+
+                    case "R":
+                        //Console.Write("Enter name of KeyVault: "); vaultName = Console.ReadLine();
+                        Console.Write("Enter Secret name: "); secretName = Console.ReadLine();
+                        string secret = kvHelper.GetSecret(secretName).GetAwaiter().GetResult();
+                        
+                        Console.WriteLine("Got Secret: " + secret);
+                        Console.Read();
+                        break;
+                    case "W":
+                        Console.Write("Enter name of Secret: "); secretName = Console.ReadLine();
+                        Console.Write("Enter Secret Value: "); secretValue = Console.ReadLine();
+
+                        kvHelper.WriteSecret(secretName, secretValue);
+                        Console.WriteLine("Write complete!");
+                        //Console.Read();
+                        break;
+                }
+                Program.printDefaultInstructions();
+                input = Console.ReadLine().ToUpper();
+            }
+        }
+
+        static void printDefaultInstructions() {
+            Console.Clear();
+            Console.WriteLine("");
+            Console.WriteLine("---------");
+            Console.WriteLine("R   = Read from KeyVault");
+            Console.WriteLine("W   = Write to KeyVault");
+
+            Console.WriteLine("X   = Exit");
+            Console.WriteLine("---------");
+            Console.WriteLine("Please enter selection");
+        }
+
+    }
+}
